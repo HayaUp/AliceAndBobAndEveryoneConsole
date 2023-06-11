f6449eccb96dc2b83bb00ab24e9b7dc80edfe916
HayaUp <hayaup.git@gmail.com>
2023-06-12 04:18:27 +0900

    A から Z までの適当な名称をまとめた。

--- AliceAndBobAndEveryoneConsole/Program.cs
+++ AliceAndBobAndEveryoneConsole/Program.cs
@@ -1,12 +1,69 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AliceAndBobAndEveryoneConsole
 {
+    public class User
+    {
+        public int Id;
+        public string Name;
+
+        public void Show()
+        {
+            Console.WriteLine($"{nameof(Id)} = {Id} / {nameof(Name)} = {Name}");
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var names = new string[]
+            {
+                "Alice",
+                "Bob",
+                "Carol",
+                "Charlie",
+                "Dave",
+                "Ellen",
+                "Frank",
+                "Gari",
+                "Haiku",
+                "Issac",
+                "Ivan",
+                "Justin",
+                "Katana",
+                "Last",
+                "Mallory",
+                "Marvin",
+                "Mallet",
+                "Matilda",
+                "Ninja",
+                "Oscar",
+                "Pat",
+                "Peggy",
+                "Plod",
+                "Quantum",
+                "Ramen",
+                "Steve",
+                "Trent",
+                "Trudy",
+                "Udon",
+                "Victor",
+                "Walter",
+                "Xanadu",
+                "Yakitori",
+                "Zoe",
+            };
+
+            var users = new List<User>();
+
+            for(int i = 0; i < names.Length; i++)
+            {
+                users.Add(new User { Id = i + 1, Name = names[i] });
+            }
+
+            users.ForEach(item => item.Show());
         }
     }
 }
--- README.md
+++ README.md
@@ -1,9 +1,107 @@
 # アリスとボブとみんな
 
 ダミーデータが欲しくなった。      
-A から Z まで適当な名前で用意できないか。
+A から Z まで適当な名称で用意できないか。
 
 **アリスとボブ**      
 https://ja.wikipedia.org/wiki/%E3%82%A2%E3%83%AA%E3%82%B9%E3%81%A8%E3%83%9C%E3%83%96
 
 コレだ！
+
+## 名称列挙
+
+```
+Alice
+Bob
+Carol
+Charlie
+Dave
+Ellen
+Frank
+Issac
+Ivan
+Justin
+Mallory
+Marvin
+Mallet
+Matilda
+Oscar
+Pat
+Peggy
+Plod
+Steve
+Trent
+Trudy
+Victor
+Walter
+Zoe
+```
+
+## 一部のアルファベットの名称が無い
+
+次のアルファベットの名称が無いことに気づいた。
+
+- G
+- H
+- K
+- L
+- N
+- Q
+- R
+- U
+- X
+- Y
+
+## 適当な名称をあてる
+
+```
+Gari
+Haiku
+Katana
+Last
+Ninja
+Quantum
+Ramen
+Udon
+Xanadu
+Yakitori
+```
+
+## 名称列挙その2
+
+```
+Alice
+Bob
+Carol
+Charlie
+Dave
+Ellen
+Frank
+Gari
+Haiku
+Issac
+Ivan
+Justin
+Katana
+Last
+Mallory
+Marvin
+Mallet
+Matilda
+Ninja
+Oscar
+Pat
+Peggy
+Plod
+Quantum
+Ramen
+Steve
+Trent
+Trudy
+Udon
+Victor
+Walter
+Xanadu
+Yakitori
+Zoe
+```
